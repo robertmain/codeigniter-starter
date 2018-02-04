2df0042a4d9a5966ed37e8e98f1dffc363f0c63d
Robert Main <lenswipe@gmail.com>
2018-02-04 15:11:15 -0500

    Database Autoloading
    Autoload the database query builder class

--- src/config/autoload.php
+++ src/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = [];
 |
 |    $autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = [];
+$autoload['libraries'] = ['database'];
 
 /*
 | -------------------------------------------------------------------
