8ec4e839224b7980b75c5122728110b0262bae1c
Robert Main <lenswipe@gmail.com>
2018-02-03 22:32:49 -0500

    Break default codeigniter view up for the template lib

--- app/views/templates/profile.php
+++ app/views/templates/profile.php
@@ -1,4 +1,15 @@
-<?php $this->layout('template', ['title' => 'User Profile']) ?>
+<?php $this->layout('template', ['title' => 'Welcome to CodeIgniter, ' . $this->e($name)]) ?>
 
-<h1>User Profile</h1>
-<p>Hello, <?=$this->e($name)?></p>
+
+<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+<p>If you would like to edit this page you'll find it located at:</p>
+<code><?php echo __FILE__; ?></code>
+
+<p>The corresponding controller for this page is found at:</p>
+<code>app\controllers\Welcome.php</code>
+
+<p>
+    If you are exploring CodeIgniter for the very first time,
+    you should start by reading the <a href="user_guide/">User Guide</a>.
+</p>
