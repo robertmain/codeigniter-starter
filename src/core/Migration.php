8001084d7115fe0a2c111935566d7847c33ae874
Robert Main <lenswipe@gmail.com>
2018-02-22 22:28:26 -0500

    Model Metadata
    Re-use the information contained in our base model about how to name our metadata datestamp fields

--- src/core/Migration.php
+++ src/core/Migration.php
@@ -3,6 +3,7 @@
 namespace App\Core;
 
 use \CI_Migration;
+use App\Core\Model;
 
 /**
  * Base Migration
@@ -12,24 +13,22 @@ use \CI_Migration;
 abstract class Migration extends CI_Migration
 {
     /**
-    * @var string The name for the field used to store record creation timestamp
-    */
-   const CREATED = 'created_at';
-
-   /**
-    * @var string The name for the field used to store record update timestamp
-   */
-   const UPDATED = 'updated_at';
-
-   /**
-    * @var string The name for the field used to store record update timestamp
-   */
-    const DELETED = 'deleted_at';
-
-    /**
      * @var Array Record metadata that can be easily added to any migration
     */
-    protected $metaData = [];
+    protected $metaData = [
+        Model::CREATED => [
+            'type' => 'DATETIME',
+            'null' => false
+        ],
+        Model::UPDATED => [
+            'type' => 'DATETIME',
+            'null' => false
+        ],
+        Model::DELETED => [
+            'type' => 'DATETIME',
+            'null' => true
+        ]
+    ];
 
     /**
      * Actions required to perform the migration
@@ -40,27 +39,4 @@ abstract class Migration extends CI_Migration
      * Actions required to roll back the migration
     */
     abstract public function down();
-
-    /**
-     * Define the datetime metadata fields for inheritance in child migrations
-    */
-    public function __construct($config = [])
-    {
-        parent::__construct($config);
-
-        $this->metaData = [
-            static::CREATED => [
-                'type' => 'DATETIME',
-                'null' => false
-            ],
-            static::UPDATED => [
-                'type' => 'DATETIME',
-                'null' => false
-            ],
-            static::DELETED => [
-                'type' => 'DATETIME',
-                'null' => true
-            ]
-        ];
-    }
 }
