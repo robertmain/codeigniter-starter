3773810a030c984028946ccd7a4fab37bb3127ac
Robert Main <lenswipe@gmail.com>
2018-02-23 00:21:30 -0500

    Flush Database
    Add drop database and re-create database functionality using the migrations and some composer scripts

--- src/controllers/cli/Migrate.php
+++ src/controllers/cli/Migrate.php
@@ -1,5 +1,7 @@
 <?php
 
+use Exceptions\Operation\InvalidOperationException;
+
 /**
  * Migration CLI controller
  *
@@ -24,5 +26,22 @@ class Migrate extends CI_Controller
     public function latest()
     {
         $this->migration->latest();
+    }
+
+    /**
+     * Drop the _ENTIRE_ database and re-create it. BE VERY CAREFUL WITH THIS.
+     *
+     * This method will not allow itself to be run if it detects that it is in a production environment
+    */
+    public function drop()
+    {
+        if (ENVIRONMENT === 'production') {
+            throw new InvalidOperationException('Can\'t drop the database in production');
+        } else {
+            $database_name = $this->db->database;
+
+            $this->dbforge->drop_database($database_name);
+            $this->dbforge->create_database($database_name);
+        }
     }
 }
