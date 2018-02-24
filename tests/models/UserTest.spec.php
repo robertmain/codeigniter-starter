175bdf838edd5703130a11a2d5c7fcc3bc72520a
Robert Main <lenswipe@gmail.com>
2018-02-23 23:03:47 -0500

    User is denied login if the password is incorrect

--- tests/models/UserTest.spec.php
+++ tests/models/UserTest.spec.php
@@ -12,12 +12,19 @@ class User extends TestCase
      */
     private $user_model;
 
-    /***/
-    public function __construct()
-    {
-        parent::__construct();
+    /**
+     * @var Object
+     */
+    private $test_user;
 
+    public function setUp()
+    {
         $this->user_model = Mockery::mock(UserModel::class)->makePartial();
+
+        $this->test_user = (object)[
+            'username' => 'mrtesting',
+            'password' => password_hash('abc', PASSWORD_DEFAULT)
+        ];
     }
 
     /**
@@ -25,14 +32,9 @@ class User extends TestCase
     */
     public function is_granted_login_with_a_valid_username_and_password()
     {
-        $user = (object)[
-            'username' => 'mrtesting',
-            'password' => password_hash('abc', PASSWORD_DEFAULT)
-        ];
-
         $this->user_model->shouldReceive('get_by')
                          ->with(['username' => 'mrtesting'])
-                         ->andReturn($user);
+                         ->andReturn($this->test_user);
 
         $valid = $this->user_model->password_verify('mrtesting', 'abc');
 
@@ -44,11 +46,6 @@ class User extends TestCase
      */
     public function is_denied_login_if_username_is_incorrect_or_does_not_exist()
     {
-        $user = (object)[
-            'username' => 'mrtesting',
-            'password' => password_hash('abc', PASSWORD_DEFAULT)
-        ];
-
         $this->user_model->shouldReceive('get_by')
                          ->with(['username' => 'dontexist'])
                          ->andReturn(null);
@@ -57,6 +54,21 @@ class User extends TestCase
 
         $this->assertFalse($valid);
     }
+
+    /**
+     * @test
+     */
+    public function is_denied_login_if_password_is_incorrect()
+    {
+        $this->user_model->shouldReceive('get_by')
+                         ->with(['username' => 'mrtesting'])
+                         ->andReturn($this->test_user);
+
+        $valid = $this->user_model->password_verify('mrtesting', 'def');
+
+        $this->assertFalse($valid);
+    }
+
     public function tearDown()
     {
         Mockery::close();
